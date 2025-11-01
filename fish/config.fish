40fae4aa007fbf36309f517e25cbe24823904cf5
Jerahmeel Cosinas <contact@jerahmeelcosinas.net>
2025-11-01 20:19:13 +1100

    initial setup

---
+++ fish/config.fish
@@ -0,0 +1,39 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here 
+    source "$HOME/.cargo/env.fish"
+
+    fish_add_path ~/.cargo/bin
+    fish_add_path /opt/homebrew/bin
+    fish_add_path /usr/local/bin
+    fish_add_path ~/.local/bin
+
+    alias ls='eza --icons'
+    alias ll='eza -l --icons --git'
+    alias la='eza -la --icons --git'
+    alias cat='bat'
+    alias find='fd'
+    alias grep='rg'
+
+    set -Ux CARAPACE_BRIDGES 'zsh,fish,bash,inshellisense' # optional
+    carapace _carapace | source
+    starship init fish | source
+    zoxide init fish | source
+
+    screenfetch
+end
+
+function nvimf
+    set selected_file (fd --type f --hidden --exclude .git | fzf --preview 'bat --color=always --style=numbers --line-range=:500 {}')
+    if test -n "$selected_file"
+        nvim $selected_file
+    end
+end
+
+function yy
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if set cwd (cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+end
---
+++ sketchybar/environment
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+PLUGIN_DIR="$CONFIG_DIR/plugins"
+ITEM_DIR="$CONFIG_DIR/items"
+THEME_DIR="$CONFIG_DIR/themes"
+
